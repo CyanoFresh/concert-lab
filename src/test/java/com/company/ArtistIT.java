9787f72e2ba53d3580b4b1a5a6eac4739e048bc7
CyanoFresh <cyanofresh@gmail.com>
2020-12-22 16:52:12 +0200

    enhanced spy

--- src/test/java/com/company/ArtistIT.java
+++ src/test/java/com/company/ArtistIT.java
@@ -8,14 +8,12 @@ import static org.mockito.Mockito.*;
 public class ArtistIT {
     @Test
     public void ArtistPersonsShouldTeamPersonsCountPlusOne() {
-        var team = spy(new ArtistTeam(10, true));
+        int personsCount = 5;
+        var team = spy(new ArtistTeam(personsCount, true));
         var artist = new Dancer("Test Name", "098", 10, team, "hip-hop");
 
-        var expectedPersonsCount = 5;
+        assertEquals(artist.getPersonsCount(), personsCount + 1);
 
-        when(team.getPersonsCount()).thenReturn(expectedPersonsCount);
-
-        assertEquals(artist.getPersonsCount(), expectedPersonsCount + 1);
         verify(team).getPersonsCount();
     }
 }
